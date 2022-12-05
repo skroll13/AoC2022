c6330752e06f0d64d3caf1b96ce38f2121c83afa
Shannon Kroll <71792819+skroll13@users.noreply.github.com>
2022-12-05 14:58:05 -0700

    solved day one, part one

--- app.js
+++ app.js
@@ -46,15 +46,32 @@
 
 var fs = require('fs');
 
-var array = fs.readFileSync('20221201_01_testData.txt').toString().split('\n')
+var array = fs.readFileSync('20221201_01_fullData.txt').toString().split('\n')
 for(i in array){
     console.log(array[i])
 }
 
-// var sum = 0;
-// for(i=0; i<array.length; i++){
-//     sum += array[i]
-// }
-// console.log(sum)
+var sum = 0;
+var max = 0;
+for(i=0; i<array.length; i++){
+
+    if(array[i] == ""){
+        console.log(sum)
+        
+        if(max < sum){
+            max = sum 
+        }
+
+        sum = 0
+
+        continue
+    }
+    
+    sum += parseInt(array[i])
+    
+}
+console.log(sum)
+console.log(max)
+
 // result: 010002000300040005000600070008000900010000
 
