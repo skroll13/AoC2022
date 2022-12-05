f6436d16e18a91f2f4af564360b44aa5931ad393
Shannon Kroll <71792819+skroll13@users.noreply.github.com>
2022-12-05 07:57:26 -0700

    pulled the data into the node.js file

--- 20221201_01_Solution.js
+++ 20221201_01_Solution.js
@@ -1,14 +1,13 @@
-1000
-2000
-3000
+// to start:
+// import txt file
+// parse data into an array
+// array of strings?
+// add each string, or whatever the small sets are, up
+// compare each string, or whatever the small sets are, to each other to find the largest in the group
 
-4000
+// ideas:
+// from Loren:
+// it was kind of a 2D array separated by double line breaks, though I processed it into one number per elf while I was reading in the file
+// so, add the first number to the second number and then add that to the third number, and so on, until you hit a line break. 
+// then, stop adding. after the line break, add the numbers together again. repeat
 
-5000
-6000
-
-7000
-8000
-9000
-
-10000
---
+++ app.js
@@ -0,0 +1,60 @@
+// to start:
+// DONE - import txt file
+// DONE? - parse data into an array - https://stackoverflow.com/questions/6831918/node-js-read-a-text-file-into-an-array-each-line-an-item-in-the-array
+// array of strings?
+// add each string, or whatever the small sets are, up
+// compare each string, or whatever the small sets are, to each other to find the largest in the group
+
+// ideas:
+// from Loren:
+// it was kind of a 2D array separated by double line breaks, though I processed it into one number per elf while I was reading in the file
+// so, add the first number to the second number and then add that to the third number, and so on, until you hit a line break. 
+// then, stop adding. after the line break, add the numbers together again. repeat
+
+
+
+
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('hello world');
+// })
+
+// server.listen(port, hostname, () =>{
+//     console.log(`server running at http:${hostname}:${port}/`)
+// })
+
+
+// this code brings in the file system - thus, fs
+// const fs = require('fs');
+
+// this code pulls in the test data.
+// it wasn't pulling in before because I had a typo in the name of the file 
+// fs.readFile('20221201_01_testData.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+var fs = require('fs');
+
+var array = fs.readFileSync('20221201_01_testData.txt').toString().split('\n')
+for(i in array){
+    console.log(array[i])
+}
+
+// var sum = 0;
+// for(i=0; i<array.length; i++){
+//     sum += array[i]
+// }
+// console.log(sum)
+// result: 010002000300040005000600070008000900010000
+
